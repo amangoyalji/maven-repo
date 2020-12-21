764494b3902082d44efe5985e71b456b7bea5988
amangoyalji <amana6398@gmail.com>
2020-12-21 15:16:53 +0530

    prod is added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -9,12 +9,17 @@ public class Calc {
 	public static int subs(int num1,int num2) {
 		return num1-num2;
 	}
+	
+	public static int prod(int num1,int num2) {
+		return num1*num2;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("sum "+add(10,20));
 		System.out.println("subtraction is "+subs(10,20));
+		System.out.println("product is "+prod(10,20));
 
 	}
 
