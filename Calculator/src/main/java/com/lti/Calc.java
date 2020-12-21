49303c40a11c9a11a8aa4cd97553ba3791daba72
amangoyalji <amana6398@gmail.com>
2020-12-21 14:59:16 +0530

    second commit

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,11 +5,16 @@ public class Calc {
 	public static int add(int num1,int num2) {
 		return num1+num2;
 	}
+	
+	public static int subs(int num1,int num2) {
+		return num1-num2;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(add(10,20));
+		System.out.println("sum "+add(10,20));
+		System.out.println("subtraction is "+subs(10,20));
 
 	}
 
